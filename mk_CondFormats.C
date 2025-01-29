a58d65bfeb2ccd0cbc10e9b7c31bd512b6154e8e
blehtela <142437787+blehtela@users.noreply.github.com>
2025-01-29 12:12:45 +0100

    Adding script to compile CondFormats libraries, just as is done in gamjet code. This is the same script, just adjusted for DiGamma.

---
+++ mk_CondFormats.C
@@ -0,0 +1,17 @@
+// Run this script to compile CondFormats libraries. After this can easily run 
+// root -l -b -q mk_DiGamHistosFill.C
+// using R__LOAD_LIBRARY to load *.so
+{
+
+  gROOT->ProcessLine(".L CondFormats/JetMETObjects/src/Utilities.cc+");
+  gROOT->ProcessLine(".L CondFormats/JetMETObjects/src/JetCorrectorParameters.cc+");
+  gROOT->ProcessLine(".L CondFormats/JetMETObjects/src/SimpleJetCorrector.cc+");
+  gROOT->ProcessLine(".L CondFormats/JetMETObjects/src/FactorizedJetCorrector.cc+");
+  
+  gROOT->ProcessLine(".L CondFormats/JetMETObjects/src/SimpleJetCorrectionUncertainty.cc+");
+  gROOT->ProcessLine(".L CondFormats/JetMETObjects/src/JetCorrectionUncertainty.cc+");
+
+  // For Gamjet code (v6.26.06)
+  gROOT->ProcessLine(".L DiGamHistosFill.C+g");
+
+}
